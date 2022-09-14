1f4dfc7736631eb5ce3ec63ca98b0404b07e87ff
Joy Nichols <joynichols@Joys-MacBook-Air.local>
2022-09-13 19:06:30 -0500

    lists

---
+++ client/src/components/lists/ListShow.js
@@ -0,0 +1,38 @@
+
+import { useState } from 'react';
+import ListForm from './ListForm';
+
+const ListShow = ({ id, title, desc, updateList }) => {
+  const [editing, setEdit] = useState(false)
+
+  return (
+    <>
+      { editing ?
+        <>
+          <ListForm 
+            updateList={updateList}
+            id={id}
+            title={title}
+            desc={desc}
+            setEdit={setEdit}
+          />
+          <button onClick={() => setEdit(false)}>
+            Cancel
+          </button>
+        </>
+        :
+        <div>
+          <h1> {title} {desc}</h1>
+          <button onClick={() => setEdit(true)}>
+            Edit
+          </button>
+          <button>Delete</button>
+          <button>Todos</button>
+        </div>
+      }
+      <hr />
+    </>
+  )
+}
+
+export default ListShow;
